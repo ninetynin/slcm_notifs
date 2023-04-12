9a42c85ec70d26d10c77a4f897044adf75e17af4
ninetynin <wekandmusic@gmail.com>
2023-04-13 00:34:34 +0530

    eeeeeeeeeee uuuuuuuuuuuu

--- src/main.py
+++ src/main.py
@@ -2,12 +2,20 @@ import os
 from dotenv import load_dotenv
 import asyncio
 from pyppeteer import launch
+import keras_ocr
 
 def setup_env():
     load_dotenv()
     SLCM_USERNAME = os.getenv("SLCM_USERNAME")
     SLCM_PASSWORD = os.getenv("SLCM_PASSWORD")
     return SLCM_USERNAME, SLCM_PASSWORD
+
+async def decode_captcha(img_path):
+    pipeline = keras_ocr.pipeline.Pipeline()
+    img = keras_ocr.tools.read(img_path)
+    for text, box in pipeline.recognize([img])[0]:
+        print(text, box)
+    return text
 
 async def login():
     SLCM_USERNAME, SLCM_PASSWORD = setup_env()
@@ -18,19 +26,24 @@ async def login():
     await page.type('input[name="txtpassword"]', SLCM_PASSWORD)
     img = await page.querySelector('img[id="imgCaptcha"]')
     img_src = await page.evaluate('(img) => img.src', img)
-    page = await browser.newPage()
-    await page.goto(img_src)
+    page2 = await browser.newPage()
+    await page2.goto(img_src)
     # <img style="-webkit-user-select: none;" src="https://slcm.manipal.edu/GenerateCaptcha.aspx?638169415399257246">
-    # there is no class or image id just go to the img element and store it in a variable
-    fin_img = await page.querySelector('img')
-    # save the image
-    await fin_img.screenshot({'path': 'captcha.png'})
-    while True:
-        #do nothing
-        pass
+    fin_img = await page2.querySelector('img')
+    rel_path = '../images/captcha.png'
+    abs_path = os.path.join(os.path.dirname(__file__), rel_path)
+    await fin_img.screenshot({'path': abs_path})
+    page2.close()
+    captcha = await decode_captcha(abs_path)
+    await page.type('input[name="txtCaptcha"]', captcha)
+    await page.click('input[name="btnLogin"]')
+    #its working finally lets go 😭 fuck selenium, pypupetteeeeeer ftw
 
 async def main():
     await login()
+    while True:
+        #do nothing
+        pass
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())

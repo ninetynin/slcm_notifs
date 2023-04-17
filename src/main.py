ab448585bff28b7e04c24461ef87dfad88da28f0
ninetynin <wekandmusic@gmail.com>
2023-04-17 19:41:24 +0530

    i kinda hate playwright

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
-import os
 import asyncio
+from playwright.async_api import async_playwright
 
-def main():
-    print("Hello World")
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        page = await browser.new_page()
+        await page.goto("https://www.google.com")
+        await page.screenshot(path="example.png")
+        while True:
+            await asyncio.sleep(1)
+
+asyncio.run(main())

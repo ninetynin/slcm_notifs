b2e49862b6a82201a1eb9f48e51d5a9e36038a69
ninetynin <wekandmusic@gmail.com>
2023-04-18 01:45:01 +0530

    debug comments added but in a cringe way unlike rust

--- src/main.py
+++ src/main.py
@@ -2,12 +2,20 @@ import asyncio
 from pyppeteer import launch
 # from instagrapi import Client 
 
+print("entered into script")
+
 async def main():
+    print("entered main fn")
     # browser = await launch(headless=False)
     browser = await launch()
+    print("browser launched")
     page = await browser.newPage()
+    print("new page created")
     await page.goto('https://slcm.manipal.edu')
+    print("page loaded")
     print(await page.title())
+    print("page title printed")
     await browser.close()
+    print("browser closed")
 
 asyncio.get_event_loop().run_until_complete(main())

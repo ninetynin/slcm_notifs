a06fc9713c92cc171c8115bda0df04857f7aa582
ninetynin <wekandmusic@gmail.com>
2023-04-17 20:05:56 +0530

    fixed error1 lets see how many more

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ from pyppeteer import launch
 # from instagrapi import Client 
 
 async def main():
-    browser = await launch('''headless=False''')
+    # browser = await launch(headless=False)
+    browser = await launch()
     page = await browser.newPage()
     await page.goto('https://slcm.manipal.edu')
     print(await page.title())

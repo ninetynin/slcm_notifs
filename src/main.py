9f4eb6710743d786deeb85daa4a9e1c69ac26bce
ninetynin <wekandmusic@gmail.com>
2023-04-20 20:35:13 +0530

    backup push fml

--- src/main.py
+++ src/main.py
@@ -1,12 +1,39 @@
 import asyncio
 import logging
-from playwright.async_api import async_playwright
 from scraper.slcm_login import slcm_login
+import os
+from dotenv import load_dotenv
+
+def setup_env(env_fn):
+    SLCM_USERNAME = os.getenv("SLCM_USERNAME")
+    SLCM_PASSWORD = os.getenv("SLCM_PASSWORD")
+    if SLCM_USERNAME == None or SLCM_PASSWORD == None:
+        print("One or more env variables are not set")
+        exit(1)
+    return SLCM_USERNAME, SLCM_PASSWORD
+
+def setup_insta_env(env_fn):
+    INSTA_USERNAME = os.getenv("INSTAGRAM_USERNAME")
+    INSTA_PASSWORD = os.getenv("INSTAGRAM_PASSWORD")
+    if INSTA_USERNAME == None or INSTA_PASSWORD == None:
+        print("One or more env variables are not set")
+        exit(1)
+    return INSTA_USERNAME, INSTA_PASSWORD
 
 async def main():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    env_fn = load_dotenv()
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        filename='logs/logs.log'
+    )
     logger = logging.getLogger(__name__)
-    slcm_login()
+    logger.info("Starting main.py")
+    SLCM_USERNAME, SLCM_PASSWORD = setup_env(env_fn)
+    INSTA_USERNAME, INSTA_PASSWORD = setup_insta_env(env_fn)
+    logger.info("Setting up slcm_login")
+    slcm = await slcm_login(SLCM_USERNAME, SLCM_PASSWORD, logger).main()
 
 if __name__ == '__main__':
     asyncio.run(main())
--- src/scraper/slcm_login.py
+++ src/scraper/slcm_login.py
@@ -1,20 +1,53 @@
 import asyncio
 import logging
 from playwright.async_api import async_playwright
-
+import os
+import keras_ocr
 
 class slcm_login:
-    def __init__(self, username, password):
+    def __init__(self, username, password, logger):
         self.username = username
         self.password = password
+        self.logger = logger
+        self.loop = asyncio.get_event_loop()
 
-    async def main(self):
-        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-        logger = logging.getLogger(__name__)
+    async def decode_captcha(self, img_path):
+        pipeline = keras_ocr.pipeline.Pipeline()
+        img = keras_ocr.tools.read(img_path)
+        for text, box in pipeline.recognize([img])[0]:
+            # print(text)
+            self.logger.info('captcha decoded')
+        self.logger.info(text)
+        return text
+
+    async def login(self):
         async with async_playwright() as p:
             browser = await p.chromium.launch(headless=False)
-            page = await browser.new_page()
-            await page.goto("https://slcm.manipal.edu")
-            await page.click("text=Login")
-            await page.fill("input[name=\"username\"]", self.username)
-            await page.fill("input[name=\"password\"]", self.passwor
+            context = await browser.new_context()
+            self.logger.info("Browser launched")
+            # page = await browser.new_page()
+            page = await context.new_page()
+            self.logger.info("New page opened")
+            await page.goto("https://slcm.manipal.edu")  
+            img_url = await page.locator('#imgCaptcha').get_attribute('src')
+            self.logger.info("Captcha url: " + img_url)
+            img_url = 'https://slcm.manipal.edu/' + img_url
+            img_tab = await context.new_page()
+            img_tab.goto(img_url)
+            await img_tab.goto(img_url)
+            abs_path = os.path.join(os.path.dirname(__file__), '../../images/captcha.png')
+            await img_tab.get_by_role('img').screenshot(path=abs_path)
+            self.logger.info("Captcha saved")
+            await img_tab.close()
+            captcha = await self.decode_captcha(abs_path)
+            await page.locator('#txtUserid').type(self.username)
+            await page.locator('#txtpassword').type(self.password)
+            await page.locator('#txtCaptcha').type(captcha)
+            # await page.get_by_role("btnLogin").click()
+            await page.click('#btnLogin')
+            while True:
+                pass
+        return page
+
+    async def main(self):
+        return await self.login()

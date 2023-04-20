734be87d1ed5ce53227f724276a6d929377d6aa8
ninetynin <wekandmusic@gmail.com>
2023-04-20 16:58:17 +0530

    ok m1 test starts now

--- src/main.py
+++ src/main.py
@@ -1,13 +1,26 @@
 import asyncio
+import logging
 from playwright.async_api import async_playwright
 
-async def main():
-    async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)
-        page = await browser.new_page()
-        await page.goto("https://www.google.com")
-        await page.screenshot(path="example.png")
-        while True:
-            await asyncio.sleep(1)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
-asyncio.run(main())
+async def main():
+    logger.info('Entered main function')
+    async with async_playwright() as p:
+        try:
+            browser = await p.chromium.launch()
+            logger.info('Browser launched')
+            page = await browser.new_page()
+            logger.info('New page created')
+            await page.goto("https://www.google.com")
+            logger.info(f'Page title: {await page.title()}')
+            await browser.close()
+            logger.info('Browser closed')
+        except Exception as e:
+            logger.error(f'Error occurred: {e}')
+            await browser.close()
+            logger.info('Browser closed')
+
+if __name__ == '__main__':
+    asyncio.run(main())
